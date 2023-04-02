402d7f111e2601399923395dcbcb25b21e0429aa
SaayaAchyty <achyty_saaya@mail.ru>
2023-04-03 00:03:51 +0700

    Add_homework4_2.04.2023

---
+++ ConsoleApp4/Counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Counter
+    {
+        public delegate void Handler();
+        public event Handler? Notify;
+        public void counter() 
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(i);
+                if (i==77)
+                {
+                    Notify?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Handler1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Handler1
+    {
+        public void DisplayMessage ()
+        {
+            Console.WriteLine ("Пора действовать, ведь уже 77");
+        }
+    }
+}
